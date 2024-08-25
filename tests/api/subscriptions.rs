a775159051dad6a90eaffcfd9a0f133051ab1fa1
r-tamura <rtamura30@gmail.com>
2024-08-25 07:17:08 +0000

    8.5 Who Should Log Errors?
    - ログはエラーが発生した場所や中継場所ではなく、エラーをハンドルするところで出力すべき

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -159,6 +159,6 @@ async fn subscribe_fails_if_there_is_a_fatal_database_error() {
     // Act
     let response = app.post_subscriptions(body.into()).await;
 
-    // Assert
+    // // Assert
     assert_eq!(response.status().as_u16(), 500);
 }
